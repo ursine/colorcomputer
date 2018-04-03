5604470cdfa1d9ab496367159f96cebe648bdfc2
Gary Coulbourne <bear@bears.org>
2018-04-03 01:24:01 +0000

    adding lwtools

---
+++ originals/lwtools-4.15/lwasm/unicorns.d
@@ -0,0 +1,11 @@
+lwasm/unicorns.o lwasm/unicorns.d: lwasm/unicorns.c lwasm/input.h lwasm/lwasm.h lwlib/lw_expr.h \
+ lwlib/lw_stringlist.h lwlib/lw_stack.h lwlib/lw_version.h \
+ lwlib/lw_alloc.h
+lwasm/unicorns.c:
+lwasm/input.h:
+lwasm/lwasm.h:
+lwlib/lw_expr.h:
+lwlib/lw_stringlist.h:
+lwlib/lw_stack.h:
+lwlib/lw_version.h:
+lwlib/lw_alloc.h:
---
+++ originals/lwtools-4.15/lwlink/main.d
@@ -0,0 +1,7 @@
+lwlink/main.o lwlink/main.d: lwlink/main.c lwlib/lw_cmdline.h lwlib/lw_version.h \
+ lwlink/lwlink.h lwlink/expr.h
+lwlink/main.c:
+lwlib/lw_cmdline.h:
+lwlib/lw_version.h:
+lwlink/lwlink.h:
+lwlink/expr.h:
